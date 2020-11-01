f572e2f932d1b7ba134eebcc122a3412b38b93bd
Dani <45202338+xsiro@users.noreply.github.com>
2020-11-01 13:15:30 +0100

    fixed about bug

--- ClockWork_Engine/ModuleGui.cpp
+++ ClockWork_Engine/ModuleGui.cpp
@@ -193,7 +193,7 @@ update_status ModuleGui::Update(float dt)
 				
 			}
 
-			ImGui::EndMenu();
+			
 		}
 		ImGui::EndMainMenuBar();
 
@@ -345,28 +345,7 @@ update_status ModuleGui::Update(float dt)
 		}
 
 	}
-	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-
-	//static float f = 0.0f;
-	//static int counter = 0;
-
-	//ImGui::Begin("Clockwork_engine");                          // Create a window called "Hello, world!" and append into it.
-
-	//ImGui::Text("YES");               // Display some text (you can use a format strings too)
-	//ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-
-	//ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-	//ImGui::ColorEdit3("Background color", (float*)&clear_color); // Edit 3 floats representing a color
-
-	//if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-	//	counter++;
-	//ImGui::SameLine();
-	//ImGui::Text("counter = %d", counter);
-
-	//ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-	//ImGui::End();
-
-
+	
 	// Rendering
 	
 	//glViewport(0, 0, (int)io->DisplaySize.x, (int)io->DisplaySize.y);
